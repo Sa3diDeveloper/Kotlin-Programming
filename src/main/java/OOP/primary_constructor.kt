6d2a5d25e94200d36d5ef35c13b7fa6c434bd8b5
sa3diProgrammer <sa3dideveloper@gmail.com>
2023-06-13 23:25:07 -0700

    Primary Constructor

--- src/main/java/OOP/primary_constructor.kt
+++ src/main/java/OOP/primary_constructor.kt
@@ -13,6 +13,8 @@ Person(name="saadi", age =23)
 }
 
 
+//A primary constructor in Kotlin is a special member function that is called when an object is instantiated (created). It is used to initialize the class properties.
+
 class Person(val name: String, val age: Int) {
     init {
         println("Person created with name $name and age $age")
