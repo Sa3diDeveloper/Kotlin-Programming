fe55fe8a6f47a18dcd2a2956c3defa9b46cf1da0
sa3diProgrammer <sa3dideveloper@gmail.com>
2023-06-13 23:44:50 -0700

    Secondary Constructors1

--- src/main/java/OOP/secondary_constructors.kt
+++ src/main/java/OOP/secondary_constructors.kt
@@ -1,2 +1,14 @@
 package OOP
 
+class Person3(val name: String) {
+
+    constructor(name: String, age: Int) : this(name) {
+        // This code is executed after the primary constructor is called.
+        println("The person's age is $age")
+    }
+}
+
+
+fun main(){var ob=Person3("Saadi",25)
+    println(ob.name)
+}
